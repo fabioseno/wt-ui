e5f3f07979a79181ea479ad44e0cdbe18a5621d1
Fábio Akira Yoshida <fabioseno@gmail.com>
2014-12-25 13:09:28 -0200

    Loader sample

--- components/dynamic-slider/sample/slider.js
+++ components/dynamic-slider/sample/slider.js
@@ -4,5 +4,9 @@ angular.module('slider', ['dynamic-slider']);
 angular.module('slider').controller('sliderController', ['$scope', function ($scope) {
     'use strict';
     
-    $scope.pages = ['PAGE 1', 'PAGE 2', 'PAGE 3'];
+    $scope.pages = [
+        {content: 'PAGE 1', color: '#ff0000'},
+        {content: 'PAGE 2', color: '#00ff00'},
+        {content: 'PAGE 3', color: '#0000ff'}
+    ];
 }]);
